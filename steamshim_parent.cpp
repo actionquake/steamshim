f866cb2ef0ecc957fc0b22cb9271f7df5b91e7b8
bAron <spam@davidbaron.se>
2022-09-06 00:15:54 +0200

    test new cvars

--- steamshim_parent.cpp
+++ steamshim_parent.cpp
@@ -197,8 +197,6 @@ static bool launchChild(ProcessType *pid)
     static bool steamCloudUser = SteamRemoteStorage()->IsCloudEnabledForAccount();
     char buf[256];
     snprintf(buf, sizeof buf, "%llu", SteamID);
-    snprintf(buf, sizeof(buf), ", +set isCloudEnabledForSteamApp %d, +set isCloudEnabledForSteamAccount %d", steamCloudApp, steamCloudUser); // Append to buffer
-
 
     // we're the child.
     GArgv[0] = strdup("q2pro");
